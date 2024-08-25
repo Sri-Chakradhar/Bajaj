5950698dd94205b008cffde6c2e590edc8ea61a2
Chakradhar <gsrichakradhar1@gmail.com>
2024-08-25 15:48:27 +0530

    version5

--- src/Solution.js
+++ src/Solution.js
@@ -1,18 +1,50 @@
 import React from "react";
 import Filter from "./components/filter.js"
+import { useState } from "react";
 import styles from './index.css';
+import Form from './components/form.js'
+import handleSubmit from "./components/form.js";
 
+import Axios from "axios"
 
 const Solution = () => {
+
+    const [data,setData]=useState("")
+
+    function handleSubmit () {
+           console.log("Called");
+           console.log(data);
+    
+           Axios.post("https://bjajbck-production.up.railway.app/bfhl",{data}).then((res)=>{
+            console.log(res);
+           })
+           
+        // fetch('https://bjajbck-production.up.railway.app/bfhl', {
+        //         method: 'POST',
+        //         headers:{
+        //              'Content-Type': 'application/json'
+        //           },
+        //         //   body: JSON.stringify(this.state)
+        //           body: JSON.stringify(data)
+        //           }).then((response) => response.json())
+                 
+        //     .catch((error) =>  console.log(error.message));
+         };
+
     return (
         <div className="border-spacing-2 border-b-slate-800 font-semibold text-white">
-            <form action="" className="flex justify-center p-36 border-red-600">
+            <form  className="flex justify-center p-36 border-red-600">
                 <div className="flex">
                 <label className="">
                      API REQUEST 
-                    <input name="string"  className="m-5"/>
+                    <input name="application/json" onChange={(e)=>{
+                        setData(e.target.value)
+                    }}  className="m-5 text-black"/>
                 </label>
-                <button type="sumbit">
+                <button onClick={(e)=>{
+                    e.preventDefault()
+                    handleSubmit()
+                }}>
                     <h1>Submit</h1>
                 </button>
                 </div>
